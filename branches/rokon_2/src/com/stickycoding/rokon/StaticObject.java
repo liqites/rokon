35db70e6d786f9692e66ad2209e22c3d46ae3975
rtaylor205 <rtaylor205@84f797ac-9f24-11de-b98f-4500b42dc123>
2010-05-14 14:28:03 +0000

    StaticObject.java

--- branches/rokon_2/src/com/stickycoding/rokon/StaticObject.java
+++ branches/rokon_2/src/com/stickycoding/rokon/StaticObject.java
@@ -7,5 +7,161 @@ package com.stickycoding.rokon;
  *
  */
 public class StaticObject {
+	
+	protected int x, y, width, height, rotation;
+	
+	public StaticObject(int x, int y, int width, int height, int rotation) {
+		if(width < 0 || height < 0) {
+			Debug.warning("StaticObject()", "Tried creating StaticObject with dimensions < 0");
+			return;
+		}
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.rotation = rotation;
+	}
+	
+	public StaticObject(int x, int y, int width, int height) {
+		this(x, y, width, height, 0);
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getRotation() {
+		return rotation;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void setXY(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void moveX(int x) {
+		this.x += x;
+	}
+	
+	public void moveY(int y) {
+		this.y += y;
+	}
+	
+	public void move(int x, int y) {
+		this.x += x;
+		this.y += y;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+		if(this.width < 0 || this.height < 0) {
+			Debug.warning("StaticObject.setWidth", "Dimensions < 0");
+			if(this.width < 0) this.width = 0;
+			if(this.height < 0) this.height = 0;
+		}
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+		if(this.width < 0 || this.height < 0) {
+			Debug.warning("StaticObject.setHeight", "Dimensions < 0");
+			if(this.width < 0) this.width = 0;
+			if(this.height < 0) this.height = 0;
+		}
+	}
+	
+	public void setSize(int width, int height) {
+		this.width = width;
+		this.height = height;
+		if(this.width < 0 || this.height < 0) {
+			Debug.warning("StaticObject.setSize", "Dimensions < 0");
+			if(this.width < 0) this.width = 0;
+			if(this.height < 0) this.height = 0;
+		}
+	}
+	
+	public void growWidth(int width) {
+		this.width += width;
+		if(this.width < 0 || this.height < 0) {
+			Debug.warning("StaticObject.growWidth", "Dimensions < 0");
+			if(this.width < 0) this.width = 0;
+			if(this.height < 0) this.height = 0;
+		}
+	}
+	
+	public void growHeight(int height) {
+		this.height += height;
+		if(this.width < 0 || this.height < 0) {
+			Debug.warning("StaticObject.growHeight", "Dimensions < 0");
+			if(this.width < 0) this.width = 0;
+			if(this.height < 0) this.height = 0;
+		}
+	}
+	
+	public void shrinkWidth(int width) {
+		this.width -= width;
+		if(this.width < 0 || this.height < 0) {
+			Debug.warning("StaticObject.shrinkWidth", "Dimensions < 0");
+			if(this.width < 0) this.width = 0;
+			if(this.height < 0) this.height = 0;
+		}
+	}
+	
+	public void shrinkHeight(int height) {
+		this.height -= height;
+		if(this.width < 0 || this.height < 0) {
+			Debug.warning("StaticObject.shrinkHeight", "Dimensions < 0");
+			if(this.width < 0) this.width = 0;
+			if(this.height < 0) this.height = 0;
+		}
+	}
+	
+	public void shrink(int width, int height) {
+		this.width -= width;
+		this.height -= height;
+		if(this.width < 0 || this.height < 0) {
+			Debug.warning("StaticObject.shrink", "Dimensions < 0");
+			if(this.width < 0) this.width = 0;
+			if(this.height < 0) this.height = 0;
+		}
+	}
+	
+	public void grow(int width, int height) {
+		this.width += width;
+		this.height += height;
+		if(this.width < 0 || this.height < 0) {
+			Debug.warning("StaticObject.grow", "Dimensions < 0");
+			if(this.width < 0) this.width = 0;
+			if(this.height < 0) this.height = 0;
+		}
+	}
+	
+	public void setRotation(int rotation) {
+		this.rotation = rotation;
+	}
+	
+	public void rotate(int rotation) {
+		this.rotation += rotation;
+	}
 
 }
