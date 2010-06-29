6569dc312b5d56c484dc9798989ad5bff05ee529
rtaylor205 <rtaylor205@84f797ac-9f24-11de-b98f-4500b42dc123>
2010-06-29 10:53:28 +0000

    Importing trunk_test

---
+++ examples/trunk_test/src/com/stickycoding/rokon_test/Test.java
@@ -0,0 +1,13 @@
+package com.stickycoding.rokon_test;
+
+import com.stickycoding.rokon.RokonActivity;
+import com.stickycoding.rokon.Scene;
+
+public class Test extends RokonActivity {
+
+	@Override
+	public void onCreate() {
+		new Scene();
+	}
+    
+}
